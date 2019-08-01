4e598c2364020bac19fedd2284a76d2dc4e6a627
MunozChristian110 <32554092+MunozChristian110@users.noreply.github.com>
2019-08-01 14:58:59 -0700

    Made migrations and created first few database models

--- MarkTimeSite/MarkTimeApp/models.py
+++ MarkTimeSite/MarkTimeApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EboardMember(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    eboard_position = models.CharField(max_length=20)
+    about_me = models.TextField()
+    # Place another attribute here for a key to a picture once that's figured out
+
+    def __str__(self):
+        info_string = self.first_name + " " + self.last_name
+        # info_string += "\n" + self.eboard_position
+        # info_string += "\n" + self.about_me
+        return info_string
+
+class HistoryYear(models.Model):
+    year = models.IntegerField()
+    Summary = models.TextField()
+    # Place another attribute here for keys to multiple pictures
