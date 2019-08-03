284366591f8ba9a69f536700a9fceedc856b2114
MunozChristian110 <32554092+MunozChristian110@users.noreply.github.com>
2019-08-02 17:12:05 -0700

    Made changes to EboardMember Model
    Can now only pick from a number of choices for the 'position' field

--- MarkTimeSite/MarkTimeApp/models.py
+++ MarkTimeSite/MarkTimeApp/models.py
@@ -6,7 +6,21 @@ from django.db import models
 class EboardMember(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
-    eboard_position = models.CharField(max_length=20)
+    EBOARD_POSITIONS = (
+        ('President', 'President'),
+        ('Vice President', 'Vice President'),
+        ('Director', 'Director'),
+        ('Assistant Director', 'Assistant Director'),
+        ('Secretary', 'Secretary'),
+        ('Treasurer', 'Treasurer'),
+        ('Membership Chair', 'Membership Chair'),
+        ('Librarian', 'Librarian'),
+        ('Property Manager', 'Property Manager'),
+        ('Historian', 'Historian'),
+        ('Webmaster', 'Webmaster'),
+        ('Section Leader', 'Section Leader')
+    )
+    eboard_position = models.CharField(max_length=20, choices=EBOARD_POSITIONS)
     about_me = models.TextField()
     # Place another attribute here for a key to a picture once that's figured out
 
@@ -16,7 +30,11 @@ class EboardMember(models.Model):
         # info_string += "\n" + self.about_me
         return info_string
 
+
 class HistoryYear(models.Model):
     year = models.IntegerField()
     Summary = models.TextField()
     # Place another attribute here for keys to multiple pictures
+
+    def __str__(self):
+        return "History for the year " + self.year
