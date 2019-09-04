f5afe735034db64fe27df0a8a5f652e58172c6a6
MunozChristian110 <32554092+MunozChristian110@users.noreply.github.com>
2019-09-03 18:23:42 -0700

    Added new allowed host

--- MarkTimeSite/MarkTimeSite/settings.py
+++ MarkTimeSite/MarkTimeSite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('MARK_TIME_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://mark-time-deploy-v2.herokuapp.com/']
 
 
 # Application definition
